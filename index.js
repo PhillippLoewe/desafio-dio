194237354946daa1422420313e890fbfae77d60c
PhillippLoewe <phillipp182@gmail.com>
2026-01-13 16:13:47 +0100

    Desafio Dio

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Teste para o github");
+let nome = "Kratos";
+let xp = 10000;
+let nivel = "";
+
+if (xp < 1000) {
+    nivel = "Ferro";
+}
+else if (xp <= 2000) {
+    nivel = "Bronze";
+}
+else if (xp <= 5000) {
+    nivel = "Prata";
+}
+else if (xp <= 7000) {
+    nivel = "Ouro";
+}
+else if (xp <= 9000) {
+    nivel = "Platina";
+}
+else if (xp <= 10000) {
+    nivel = "Imortal";
+}
+else {
+    nivel = "Radiante";
+}
+
+for (let i = 0; i < 1; i++) {
+    console.log(`O Herói de nome ${nome} está no nível ${nivel}`);
+}
